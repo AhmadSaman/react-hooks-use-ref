0dcd5b8c86a98c69d746df128c0c730e2086feb8
AhmadSaman <ahmad.00079821@gmail.com>
2021-08-01 19:27:14 +0300

    commit tree 319171ab647b7ff3a04c48eb5c3afaf5aa576d68

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,14 +1,19 @@
 import React, { useRef } from "react";
 
 function Box() {
-  const elementRef = useRef();
+	const elementRef = useRef();
 
-  return (
-    <div ref={elementRef}>
-      <h1>Box</h1>
-      <button>Measure</button>
-    </div>
-  );
+	function handleClick() {
+		const div = elementRef.current;
+		console.log("Measurements:", div.getBoundingClientRect());
+	}
+
+	return (
+		<div ref={elementRef}>
+			<h1>Box</h1>
+			<button onClick={handleClick}>Measure</button>
+		</div>
+	);
 }
 
 export default Box;
--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,23 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
-  const [price, setPrice] = useState(0);
-  const [color, setColor] = useState("black");
+	const [price, setPrice] = useState(0);
+	const [color, setColor] = useState("black");
+	const prevPriceRef = useRef(price);
+	useEffect(() => {
+		const prevPrice = prevPriceRef.current;
+		console.log(prevPrice, price);
+		if (prevPrice < price) {
+			setColor("green");
+		} else if (prevPrice > price) {
+			setColor("red");
+		} else {
+			setColor("black");
+		}
+		prevPriceRef.current = price;
+	}, [price]);
+	useEffect(() => {
+		const id = setInterval(() => setPrice(makeRandomNumber), 5000);
+		return function () {
+			clearInterval(id);
+		};
+	}, [price]);
 
-  useEffect(() => {
-    const id = setInterval(() => setPrice(makeRandomNumber), 1000);
-    return function () {
-      clearInterval(id);
-    };
-  }, []);
-
-  return (
-    <div>
-      <h1>TickerMaster</h1>
-      <h2 style={{ color: color }}>Price: ${price}</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h1>TickerMaster</h1>
+			<h2 style={{ color: color }}>Price: ${price}</h2>
+		</div>
+	);
 }
 
 export default Ticker;
